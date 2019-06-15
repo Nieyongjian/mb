5f3326539a64d46ed4e2b7e540656840d5f8330e
nieyj <594739273@qq.com>
2019-06-15 17:55:24 +0800

    myInfo modifiy

--- myInfo.php
+++ myInfo.php
@@ -3,21 +3,18 @@
 <head>
     <title>个人主页</title>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no">
+    <meta content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0" name="viewport"/>
+    <meta content="yes" name="apple-mobile-web-app-capable"/>
+    <meta content="black" name="apple-mobile-web-app-status-bar-style"/>
+    <meta content="telephone=no" name="format-detection"/>
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link href="css/case.css" rel="stylesheet">
     <link href="css/list.css" rel="stylesheet">
     <link href="bs/bootstrap.css" rel="stylesheet">
+    <link href="css/my.css" rel="stylesheet" type="text/css"/>
     <script src="js/jquery.js"></script>
     <script src="js/popper.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
-    <script>
-        (function(){
-            var html=document.documentElement;
-            var hWidth=html.getBoundingClientRect().width;
-            html.style.fontSize=hWidth/15+"px ";//1rem=50px
-        })();
-    </script>
     <script>
         $(window).ready( function() {
             $("#status").fadeOut();
@@ -37,7 +34,7 @@
 <div class="d-flex fixed-top">
     <h3>个人中心</h3>
 </div>
-<header class="header">
+
 
 <?php
 include "utils.php";
@@ -48,55 +45,81 @@ $nickname = $_SESSION['nickname'];
 $subscribe_time = $_SESSION['subscribe_time'];
 $subscribe_time = $utils->convert_time_nyr($subscribe_time);
 ?>
-    <section class="info">
-        <div class="headerImg"><img src=<?php echo "$headimgurl"; ?>></div>
-        <div class="userName">
-            <span class="glyphicon glyphicon-user" style="color:white;"></span>
-            <span>
-                <?php echo "$nickname"; ?>
-            </span>
-        </div>
-        <p class="regTime">注册时间：<?php echo "$subscribe_time"; ?></p>
-    </section>
-</header>
 
-
-
-    <section class="menu">
-        <a href="order.php" class="item">
-            <i class="glyphicon glyphicon-comment" style="color:#8BC34A;">&nbsp;</i>
-            <span>发布任务</span>
-        </a>
-        <a href="myorders.php" class="item">
-            <i class="glyphicon glyphicon-open" style="color:#007bff;">&nbsp;</i>
-            <span>我的发布</span>
-        </a>
-        <a href="gotorders.php" class="item">
-            <i class="glyphicon glyphicon-import" style="color:orange;">&nbsp;</i>
-            <span>我的抢单</span>
-        </a>
-
-
-    </section>
-<section class="menu2">
-
-    <a href="#" class="item">
-        <i class="glyphicon glyphicon-cog" style="color:#007aff;">&nbsp;</i>
-        <span>设置</span>
-    </a>
-    <a href="#" class="item">
-        <i class="glyphicon glyphicon-knight" style="color:yellow;">&nbsp;</i>
-        <span>关于我们</span>
-    </a>
-    <a href="#" class="item">
-        <i class="glyphicon glyphicon-inbox" style="color:#6641e2;">&nbsp;</i>
-        <span>常用地址</span>
-    </a>
-    <a href="my.php" class="item">
-        <i class="glyphicon glyphicon-piggy-bank" style="color:#1e88e5;">&nbsp;</i>
-        <span>个人账户</span>
-    </a>
-</section>
+<section class="aui-flexView">
+            <section class="aui-scrollView">
+                <div class="aui-flex-box">
+                    <div class="aui-flex-box-hd">
+                        <img src="<?php echo $headimgurl;?>" alt="">
+                    </div>
+                    <div class="aui-flex-box-bd"><?php echo $nickname;?>
+                    </div>
+                    <!-- <div class="aui-flex-box-fr">实名认证</div> -->
+                </div>
+                <div class="aui-flex-box">
+                    <div class="aui-flex-box-bd">
+                        <h2>
+                            总资产(元) 
+                        </h2>
+                        <h3>1200.99</h3>
+                    </div>
+                    <div class="aui-flex-box-fr"></div>
+                </div>
+                
+                <div class="divHeight"></div>
+                <div class="aui-icon-box">
+                    <a class="aui-flex-box" href="order.php">
+                        <div class="aui-flex-box-hd">
+                            <img src="images/fabudd.png" alt="">
+                        </div>
+                        <div class="aui-flex-box-bd">发布订单
+                        </div>
+                        <div class="aui-flex-box-fr"></div>
+                    </a>
+                    <div class="divHeight"></div>
+                    
+                    <a class="aui-flex-box" href="gotorders.php">
+                        <div class="aui-flex-box-hd">
+                            <img src="images/wdqiangdan.png" alt="">
+                        </div>
+                        <div class="aui-flex-box-bd">我的抢单
+                        </div>
+                        <div class="aui-flex-box-fr"></div>
+                    </a>
+                    <a class="aui-flex-box" href="myorders.php">
+                            <div class="aui-flex-box-hd">
+                                <img src="images/wdfabu.png" alt="">
+                            </div>
+                            <div class="aui-flex-box-bd">我的发布
+                            </div>
+                            <div class="aui-flex-box-fr"></div>
+                        </a>
+                    
+                    <div class="divHeight"></div>
+                    <a class="aui-flex-box" href="tixian.html">
+                            <div class="aui-flex-box-hd">
+                                <img src="images/tx.png" alt="">
+                            </div>
+                            <div class="aui-flex-box-bd">提现
+                            </div>
+                            <div class="aui-flex-box-fr"></div>
+                    </a>
+                    <div class="divHeight"></div>
+                    <a class="aui-flex-box" href="about.html">
+                        <div class="aui-flex-box-hd">
+                            <img src="images/gywm.png" alt="">
+                        </div>
+                        <div class="aui-flex-box-bd">关于我们
+                        </div>
+                        <div class="aui-flex-box-fr"></div>
+                    </a>
+                   
+                    
+                    <div class="divHeight"></div>
+                </div>
+            </section>
+            
+        </section>
     <!--<h3 class="title">安全设置</h3>-->
     <!--<section class="safe">-->
         <!--<a href="" class="item">-->
